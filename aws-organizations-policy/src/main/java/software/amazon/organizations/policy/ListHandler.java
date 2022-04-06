b978fb30237a5cd55dac828f6a894c7f70ef6161
Ron Lewis <ronallw@amazon.com>
2022-04-05 18:13:53 -0700

    Implement List handler for Policy resource type

--- aws-organizations-policy/src/main/java/software/amazon/organizations/policy/ListHandler.java
+++ aws-organizations-policy/src/main/java/software/amazon/organizations/policy/ListHandler.java
@@ -1,30 +1,61 @@
 package software.amazon.organizations.policy;
 
+import software.amazon.awssdk.services.organizations.OrganizationsClient;
+import software.amazon.awssdk.services.organizations.model.ListPoliciesRequest;
+import software.amazon.awssdk.services.organizations.model.ListPoliciesResponse;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.HandlerErrorCode;
 import software.amazon.cloudformation.proxy.Logger;
 import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.OperationStatus;
+import software.amazon.cloudformation.proxy.ProxyClient;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListHandler extends BaseHandler<CallbackContext> {
+
+public class ListHandler extends BaseHandlerStd {
+    private Logger logger;
 
     @Override
     public ProgressEvent<ResourceModel, CallbackContext> handleRequest(
-        final AmazonWebServicesClientProxy proxy,
+        final AmazonWebServicesClientProxy awsClientProxy,
         final ResourceHandlerRequest<ResourceModel> request,
         final CallbackContext callbackContext,
+        final ProxyClient<OrganizationsClient> orgsClient,
         final Logger logger) {
+
+        this.logger = logger;
+        logger.log(String.format("Entered %s list handler with accountId [%s]", ResourceModel.TYPE_NAME, request.getAwsAccountId()));
+
+        final ResourceModel model = request.getDesiredResourceState();
+        if (model == null || model.getType() == null) {
+            return ProgressEvent.failed(ResourceModel.builder().build(), callbackContext, HandlerErrorCode.InvalidRequest,
+                "Policies cannot be listed without specifying a type in the provided resource model!");
+        }
 
         final List<ResourceModel> models = new ArrayList<>();
 
-        // TODO : put your code here
+        return awsClientProxy.initiate("AWS-Organizations-Policy::ListPolicy", orgsClient, model, callbackContext)
+            .translateToServiceRequest(t -> Translator.translateToListPoliciesRequest(model, request.getNextToken()))
+            .makeServiceCall(this::listPolicies)
+            .handleError((organizationsRequest, e, proxyClient1, model1, context) -> handleError(
+                organizationsRequest, e, proxyClient1, model1, context, logger))
+            .done(listPoliciesResponse -> {
+                String nextToken = listPoliciesResponse.nextToken();
+                models.addAll(Translator.translateListPoliciesResponseToResourceModels(listPoliciesResponse));
+                return ProgressEvent.<ResourceModel, CallbackContext>builder()
+                    .resourceModels(models)
+                    .nextToken(nextToken)
+                    .status(OperationStatus.SUCCESS)
+                    .build();
+            });
+    }
 
-        return ProgressEvent.<ResourceModel, CallbackContext>builder()
-            .resourceModels(models)
-            .status(OperationStatus.SUCCESS)
-            .build();
+    protected ListPoliciesResponse listPolicies(final ListPoliciesRequest listPoliciesRequest, final ProxyClient<OrganizationsClient> orgsClient) {
+        logger.log("Start calling listPolicies");
+        final ListPoliciesResponse listPoliciesResponse = orgsClient.injectCredentialsAndInvokeV2(listPoliciesRequest, orgsClient.client()::listPolicies);
+        return listPoliciesResponse;
     }
 }
--- aws-organizations-policy/src/test/java/software/amazon/organizations/policy/ListHandlerTest.java
+++ aws-organizations-policy/src/test/java/software/amazon/organizations/policy/ListHandlerTest.java
@@ -1,9 +1,14 @@
 package software.amazon.organizations.policy;
 
+import software.amazon.awssdk.services.organizations.OrganizationsClient;
+import software.amazon.awssdk.services.organizations.model.ListPoliciesRequest;
+import software.amazon.awssdk.services.organizations.model.ListPoliciesResponse;
+import software.amazon.awssdk.services.organizations.model.PolicySummary;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
-import software.amazon.cloudformation.proxy.Logger;
+import software.amazon.cloudformation.proxy.HandlerErrorCode;
 import software.amazon.cloudformation.proxy.OperationStatus;
 import software.amazon.cloudformation.proxy.ProgressEvent;
+import software.amazon.cloudformation.proxy.ProxyClient;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,36 +16,57 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Duration;
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class ListHandlerTest {
+public class ListHandlerTest extends AbstractTestBase {
 
     @Mock
-    private AmazonWebServicesClientProxy proxy;
+    private AmazonWebServicesClientProxy mockAwsClientProxy;
 
     @Mock
-    private Logger logger;
+    private ProxyClient<OrganizationsClient> mockProxyClient;
+
+    @Mock
+    OrganizationsClient mockOrgsClient;
+
+    private ListHandler listHandlerToTest;
 
     @BeforeEach
     public void setup() {
-        proxy = mock(AmazonWebServicesClientProxy.class);
-        logger = mock(Logger.class);
+        listHandlerToTest = new ListHandler();
+        mockAwsClientProxy = new AmazonWebServicesClientProxy(logger, MOCK_CREDENTIALS, () -> Duration.ofSeconds(600).toMillis());
+        mockOrgsClient = mock(OrganizationsClient.class);
+        mockProxyClient = MOCK_PROXY(mockAwsClientProxy, mockOrgsClient);
     }
 
     @Test
-    public void handleRequest_SimpleSuccess() {
-        final ListHandler handler = new ListHandler();
-
-        final ResourceModel model = ResourceModel.builder().build();
-
+    public void handleRequest_ServiceControlPolicies_WithNextToken_SimpleSuccess() {
+        final ResourceModel serviceControlPolicyTypeModel = ResourceModel.builder()
+            .type(PolicyConstants.PolicyType.SERVICE_CONTROL_POLICY.toString())
+            .build();
         final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
-            .desiredResourceState(model)
+            .desiredResourceState(serviceControlPolicyTypeModel)
             .build();
 
+
+        ListPoliciesResponse listPoliciesResponse = ListPoliciesResponse.builder()
+            .policies(Arrays.asList(getMockPolicySummaryWithType(PolicyConstants.PolicyType.SERVICE_CONTROL_POLICY.toString())))
+            .nextToken(TEST_NEXT_TOKEN)
+            .build();
+        when(mockProxyClient.client().listPolicies(any(ListPoliciesRequest.class))).thenReturn(listPoliciesResponse);
+
         final ProgressEvent<ResourceModel, CallbackContext> response =
-            handler.handleRequest(proxy, request, null, logger);
+            listHandlerToTest.handleRequest(mockAwsClientProxy, request, new CallbackContext(), mockProxyClient, logger);
 
         assertThat(response).isNotNull();
         assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
@@ -48,7 +74,161 @@ public class ListHandlerTest {
         assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
         assertThat(response.getResourceModel()).isNull();
         assertThat(response.getResourceModels()).isNotNull();
+        assertThat(response.getResourceModels().stream().count()).isEqualTo(1);
+        assertThat(response.getNextToken()).isNotNull();
+        assertThat(response.getNextToken()).isEqualTo(TEST_NEXT_TOKEN);
         assertThat(response.getMessage()).isNull();
         assertThat(response.getErrorCode()).isNull();
+
+        verify(mockProxyClient.client()).listPolicies(any(ListPoliciesRequest.class));
+        verify(mockOrgsClient, atLeastOnce()).serviceName();
+        verifyNoMoreInteractions(mockOrgsClient);
+    }
+
+    @Test
+    public void handleRequest_TagPolicies_SimpleSuccess() {
+        final ResourceModel tagPolicyTypeModel = ResourceModel.builder()
+            .type(PolicyConstants.PolicyType.TAG_POLICY.toString())
+            .build();
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+            .desiredResourceState(tagPolicyTypeModel)
+            .build();
+
+
+        ListPoliciesResponse listPoliciesResponse = ListPoliciesResponse.builder()
+            .policies(Arrays.asList(getMockPolicySummaryWithType(PolicyConstants.PolicyType.TAG_POLICY.toString())))
+            .build();
+        when(mockProxyClient.client().listPolicies(any(ListPoliciesRequest.class))).thenReturn(listPoliciesResponse);
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+            listHandlerToTest.handleRequest(mockAwsClientProxy, request, new CallbackContext(), mockProxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
+        assertThat(response.getCallbackContext()).isNull();
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModel()).isNull();
+        assertThat(response.getResourceModels()).isNotNull();
+        assertThat(response.getResourceModels().stream().count()).isEqualTo(1);
+        assertThat(response.getNextToken()).isNull();
+        assertThat(response.getMessage()).isNull();
+        assertThat(response.getErrorCode()).isNull();
+
+        verify(mockProxyClient.client()).listPolicies(any(ListPoliciesRequest.class));
+        verify(mockOrgsClient, atLeastOnce()).serviceName();
+        verifyNoMoreInteractions(mockOrgsClient);
+    }
+
+    @Test
+    public void handleRequest_BackupPolicies_SimpleSuccess() {
+        final ResourceModel backupPolicyTypeModel = ResourceModel.builder()
+            .type(PolicyConstants.PolicyType.BACKUP_POLICY.toString())
+            .build();
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+            .desiredResourceState(backupPolicyTypeModel)
+            .build();
+
+
+        ListPoliciesResponse listPoliciesResponse = ListPoliciesResponse.builder()
+            .policies(Arrays.asList(getMockPolicySummaryWithType(PolicyConstants.PolicyType.BACKUP_POLICY.toString())))
+            .nextToken(null)
+            .build();
+        when(mockProxyClient.client().listPolicies(any(ListPoliciesRequest.class))).thenReturn(listPoliciesResponse);
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+            listHandlerToTest.handleRequest(mockAwsClientProxy, request, new CallbackContext(), mockProxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
+        assertThat(response.getCallbackContext()).isNull();
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModel()).isNull();
+        assertThat(response.getResourceModels()).isNotNull();
+        assertThat(response.getResourceModels().stream().count()).isEqualTo(1);
+        assertThat(response.getNextToken()).isNull();
+        assertThat(response.getMessage()).isNull();
+        assertThat(response.getErrorCode()).isNull();
+
+        verify(mockProxyClient.client()).listPolicies(any(ListPoliciesRequest.class));
+        verify(mockOrgsClient, atLeastOnce()).serviceName();
+        verifyNoMoreInteractions(mockOrgsClient);
+    }
+
+    @Test
+    public void handleRequest_AIOptOutServicesPolicies_SimpleSuccess() {
+        final ResourceModel aiOptOutPolicyTypeModel = ResourceModel.builder()
+            .type(PolicyConstants.PolicyType.AISERVICES_OPT_OUT_POLICY.toString())
+            .build();
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+            .desiredResourceState(aiOptOutPolicyTypeModel)
+            .build();
+
+
+        ListPoliciesResponse listPoliciesResponse = ListPoliciesResponse.builder()
+            .policies(Arrays.asList(getMockPolicySummaryWithType(PolicyConstants.PolicyType.AISERVICES_OPT_OUT_POLICY.toString())))
+            .build();
+        when(mockProxyClient.client().listPolicies(any(ListPoliciesRequest.class))).thenReturn(listPoliciesResponse);
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+            listHandlerToTest.handleRequest(mockAwsClientProxy, request, new CallbackContext(), mockProxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
+        assertThat(response.getCallbackContext()).isNull();
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModel()).isNull();
+        assertThat(response.getResourceModels()).isNotNull();
+        assertThat(response.getResourceModels().stream().count()).isEqualTo(1);
+        assertThat(response.getNextToken()).isNull();
+        assertThat(response.getMessage()).isNull();
+        assertThat(response.getErrorCode()).isNull();
+
+        verify(mockProxyClient.client()).listPolicies(any(ListPoliciesRequest.class));
+        verify(mockOrgsClient, atLeastOnce()).serviceName();
+        verifyNoMoreInteractions(mockOrgsClient);
+    }
+
+    @Test
+    public void handleRequest_NullDesiredModel_Fails_With_CfnInvalidRequest() {
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+            .desiredResourceState(null)
+            .build();
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+            listHandlerToTest.handleRequest(mockAwsClientProxy, request, new CallbackContext(), mockProxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.InvalidRequest);
+    }
+
+    @Test
+    public void handleRequest_NullDesiredModelType_Fails_With_CfnInvalidRequest() {
+        final ResourceModel noPolicyTypeModel = ResourceModel.builder()
+            .build();
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+            .desiredResourceState(noPolicyTypeModel)
+            .build();
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+            listHandlerToTest.handleRequest(mockAwsClientProxy, request, new CallbackContext(), mockProxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.InvalidRequest);
+    }
+
+
+    protected PolicySummary getMockPolicySummaryWithType(final String policyType) {
+        return PolicySummary.builder()
+            .name(TEST_POLICY_NAME)
+            .id(TEST_POLICY_ID)
+            .arn(TEST_POLICY_ARN)
+            .description(TEST_POLICY_DESCRIPTION)
+            .type(policyType)
+            .awsManaged(TEST_AWSMANAGED)
+            .build();
     }
 }
