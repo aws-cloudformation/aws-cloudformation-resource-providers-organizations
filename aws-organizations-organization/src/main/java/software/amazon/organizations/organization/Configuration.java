f2625d96640cea3616f30a2c455cf80a472bdc2d
shuqz <95371565+shuqz@users.noreply.github.com>
2022-04-12 14:05:06 -0700

    remove AWS::Organizations::Organization (#26)

--- aws-organizations-organization/src/main/java/software/amazon/organizations/organization/Configuration.java
+++
@@ -1,8 +0,0 @@
-package software.amazon.organizations.organization;
-
-class Configuration extends BaseConfiguration {
-
-    public Configuration() {
-        super("aws-organizations-organization.json");
-    }
-}
