d3b481532bd69c1ca93c5d54f90fef670daf2420
shuqz <95371565+shuqz@users.noreply.github.com>
2022-04-29 11:56:56 -0700

    add basic client retry strategy (#33)
    * add basic client retry strategy
    
    * add backoffstrategy

--- aws-organizations-organizationalunit/src/main/java/software/amazon/organizations/organizationalunit/ClientBuilder.java
+++ aws-organizations-organizationalunit/src/main/java/software/amazon/organizations/organizationalunit/ClientBuilder.java
@@ -1,45 +1,75 @@
 package software.amazon.organizations.organizationalunit;
 
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.core.retry.RetryPolicy;
+import software.amazon.awssdk.core.retry.backoff.BackoffStrategy;
+import software.amazon.awssdk.core.retry.backoff.EqualJitterBackoffStrategy;
+import software.amazon.awssdk.core.retry.conditions.RetryCondition;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.organizations.OrganizationsClient;
 import software.amazon.cloudformation.LambdaWrapper;
 
+import java.time.Duration;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ClientBuilder {
-  public static OrganizationsClient getClient() {
-    String region = System.getenv("AWS_REGION");
 
-    return OrganizationsClient.builder()
-            .httpClient(LambdaWrapper.HTTP_CLIENT)
-            .region(getGlobalRegion(region))
+    // Retry Strategy
+    private static final int MAX_ERROR_RETRY = 3;
+    private static final BackoffStrategy BACKOFF_STRATEGY = EqualJitterBackoffStrategy.builder()
+                                                                .baseDelay(Duration.ofMillis(500))
+                                                                .maxBackoffTime(Duration.ofMillis(5000))
+                                                                .build();
+    private static final BackoffStrategy THROTTLE_BACKOFF_STRATEGY = EqualJitterBackoffStrategy.builder()
+                                                                         .baseDelay(Duration.ofMillis(1000))
+                                                                         .maxBackoffTime(Duration.ofMillis(10000))
+                                                                         .build();
+
+    private static final RetryPolicy ORGANIZATIONS_RETRY_POLICY =
+        RetryPolicy.builder()
+            .numRetries(MAX_ERROR_RETRY)
+            .retryCondition(RetryCondition.defaultRetryCondition())
+            .backoffStrategy(BACKOFF_STRATEGY)
+            .throttlingBackoffStrategy(THROTTLE_BACKOFF_STRATEGY)
             .build();
-  }
 
-  /**
-   * Returns aws-us-gov-global Region if current region is a gov region.
-   * Returns aws-cn-global Region if current region is a china region.
-   * Otherwise returns aws-global Region
-   * @param region current AWS region {@link ResourceHandlerRequest#getRegion()}
-   * @return IAM Global Region
-   */
-  private static Region getGlobalRegion(final String region) {
-      final String currentRegion = Optional.ofNullable(region).orElse("");
-      final Pattern isGovPattern = Pattern.compile("us-gov");
-      final Pattern isChinaPattern = Pattern.compile("cn");
+    public static OrganizationsClient getClient() {
+        String region = System.getenv("AWS_REGION");
 
-      final Matcher isGovMatcher = isGovPattern.matcher(currentRegion);
-      final Matcher isChinaMatcher = isChinaPattern.matcher(currentRegion);
+        return OrganizationsClient.builder()
+                   .httpClient(LambdaWrapper.HTTP_CLIENT)
+                   .overrideConfiguration(ClientOverrideConfiguration.builder()
+                                              .retryPolicy(ORGANIZATIONS_RETRY_POLICY)
+                                              .build())
+                   .region(getGlobalRegion(region))
+                   .build();
+    }
 
-      if (isGovMatcher.find()) {
-          return Region.AWS_US_GOV_GLOBAL;
-      } else if (isChinaMatcher.find()) {
-          return Region.AWS_CN_GLOBAL;
-      }
+    /**
+     * Returns aws-us-gov-global Region if current region is a gov region.
+     * Returns aws-cn-global Region if current region is a china region.
+     * Otherwise returns aws-global Region
+     *
+     * @param region current AWS region {@link ResourceHandlerRequest#getRegion()}
+     * @return IAM Global Region
+     */
+    private static Region getGlobalRegion(final String region) {
+        final String currentRegion = Optional.ofNullable(region).orElse("");
+        final Pattern isGovPattern = Pattern.compile("us-gov");
+        final Pattern isChinaPattern = Pattern.compile("cn");
 
-      return Region.AWS_GLOBAL;
-  }
+        final Matcher isGovMatcher = isGovPattern.matcher(currentRegion);
+        final Matcher isChinaMatcher = isChinaPattern.matcher(currentRegion);
+
+        if (isGovMatcher.find()) {
+            return Region.AWS_US_GOV_GLOBAL;
+        } else if (isChinaMatcher.find()) {
+            return Region.AWS_CN_GLOBAL;
+        }
+
+        return Region.AWS_GLOBAL;
+    }
 
 }
