46ce21b46c5df5015524407f028ba0e31129e841
Shelley Zhang <shuqz@amazon.com>
2022-04-28 23:09:33 -0700

    add basic client retry strategy

--- aws-organizations-policy/src/main/java/software/amazon/organizations/policy/ClientBuilder.java
+++ aws-organizations-policy/src/main/java/software/amazon/organizations/policy/ClientBuilder.java
@@ -1,30 +1,53 @@
 package software.amazon.organizations.policy;
 
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.core.retry.RetryPolicy;
+import software.amazon.awssdk.core.retry.backoff.BackoffStrategy;
+import software.amazon.awssdk.core.retry.conditions.RetryCondition;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.organizations.OrganizationsClient;
+import software.amazon.awssdk.core.retry.backoff.EqualJitterBackoffStrategy;
 
 import software.amazon.cloudformation.LambdaWrapper;
 
+import java.time.Duration;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ClientBuilder {
-    private ClientBuilder() {}
+    // Retry Strategy
+    private static final BackoffStrategy BACKOFF_STRATEGY = EqualJitterBackoffStrategy.builder()
+                                                                .baseDelay(Duration.ofMillis(2000))
+                                                                .maxBackoffTime(Duration.ofMillis(20000))
+                                                                .build();
+    private static final RetryPolicy ORGANIZATIONS_RETRY_POLICY =
+        RetryPolicy.builder()
+            .numRetries(4)
+            .retryCondition(RetryCondition.defaultRetryCondition())
+            .throttlingBackoffStrategy(BACKOFF_STRATEGY)
+            .build();
+
+    private ClientBuilder() {
+    }
 
     public static OrganizationsClient getClient() {
         return OrganizationsClient.builder()
-             .region(getGlobalRegion())
-             .httpClient(LambdaWrapper.HTTP_CLIENT)
-             .build();
+                   .overrideConfiguration(ClientOverrideConfiguration.builder()
+                                              .retryPolicy(ORGANIZATIONS_RETRY_POLICY)
+                                              .build())
+                   .region(getGlobalRegion())
+                   .httpClient(LambdaWrapper.HTTP_CLIENT)
+                   .build();
     }
 
-  /**
-   * Returns aws-us-gov-global Region if current region is a gov region.
-   * Returns aws-cn-global Region if current region is a china region.
-   * Otherwise returns aws-global Region
-   * @return AWS Organizations Global Region
-   */
+    /**
+     * Returns aws-us-gov-global Region if current region is a gov region.
+     * Returns aws-cn-global Region if current region is a china region.
+     * Otherwise returns aws-global Region
+     *
+     * @return AWS Organizations Global Region
+     */
     private static Region getGlobalRegion() {
         final String currentRegion = Optional.ofNullable(System.getenv("AWS_REGION")).orElse("");
         final Pattern isGovPattern = Pattern.compile("us-gov");
