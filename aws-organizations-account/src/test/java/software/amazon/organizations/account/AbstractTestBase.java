1c344321a2ae9a165a3fe31fbae75636d5f4ee4e
Shelley Zhang <shuqz@amazon.com>
2024-10-01 11:06:12 -0700

    correct account schema hexstring

--- aws-organizations-account/src/test/java/software/amazon/organizations/account/AbstractTestBase.java
+++ aws-organizations-account/src/test/java/software/amazon/organizations/account/AbstractTestBase.java
@@ -50,7 +50,7 @@ public class AbstractTestBase {
     protected static final String TEST_JOINED_METHOD = "CREATED";
     protected static final Instant TEST_JOINED_TIMESTAMP = Instant.parse("2017-02-03T10:47:30.00Z");
     protected static final String ACCOUNT_JSON_SCHEMA_FILE_NAME = "aws-organizations-account.json";
-    protected static final String ACCOUNT_SCHEMA_SHA256_HEXSTRING = "C7FB7B74668A4F320299F978A5CE04E6B7952EC1B8D734D46A1AF75EFA6E2BD0";
+    protected static final String ACCOUNT_SCHEMA_SHA256_HEXSTRING = "F25AC8ED367293E5F6E354BFA4BFB6A45A3E968DD1412CF601990CA2D455FE17";
 
     protected static final DescribeAccountResponse describeAccountResponse = DescribeAccountResponse.builder().account(Account.builder()
                                                                                                           .arn(TEST_ACCOUNT_ARN)
