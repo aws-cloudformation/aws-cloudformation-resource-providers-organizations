cb6403f91a0dcd849b04c57bf23021a0b22265d9
Kavya Alla <kavyalla@amazon.com>
2025-01-16 14:15:09 -0800

    Fixed Organizations Client Builders to depend on SDK endpoint resolution instead of manual mapping

--- aws-organizations-account/src/main/java/software/amazon/organizations/account/ClientBuilder.java
+++ aws-organizations-account/src/main/java/software/amazon/organizations/account/ClientBuilder.java
@@ -16,9 +16,6 @@ import software.amazon.cloudformation.LambdaWrapper;
 
 import java.time.Duration;
 import java.util.Collections;
-import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class ClientBuilder {
     // Retry Strategy
@@ -52,34 +49,14 @@ public class ClientBuilder {
     }
 
     public static OrganizationsClient getClient() {
+        String region = System.getenv("AWS_REGION");
+
         return OrganizationsClient.builder()
                    .overrideConfiguration(ClientOverrideConfiguration.builder()
                                               .retryPolicy(ORGANIZATIONS_RETRY_POLICY)
                                               .build())
-                   .region(getGlobalRegion())
+                   .region(Region.of(region))
                    .httpClient(LambdaWrapper.HTTP_CLIENT)
                    .build();
-    }
-
-    /**
-     * Returns aws-us-gov-global Region if current region is a gov region.
-     * Returns aws-cn-global Region if current region is a china region.
-     * Otherwise returns aws-global Region
-     *
-     * @return AWS Organizations Global Region
-     */
-    private static Region getGlobalRegion() {
-        final String currentRegion = Optional.ofNullable(System.getenv("AWS_REGION")).orElse("");
-        final Pattern isGovPattern = Pattern.compile("us-gov");
-        final Matcher isGovMatcher = isGovPattern.matcher(currentRegion);
-        final Pattern isChinaPattern = Pattern.compile("cn");
-        final Matcher isChinaMatcher = isChinaPattern.matcher(currentRegion);
-
-        if (isGovMatcher.find()) {
-            return Region.AWS_US_GOV_GLOBAL;
-        } else if (isChinaMatcher.find()) {
-            return Region.AWS_CN_GLOBAL;
-        }
-        return Region.AWS_GLOBAL;
     }
 }
